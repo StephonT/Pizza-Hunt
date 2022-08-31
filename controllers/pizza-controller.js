814d3186e6f7336b5a710652790f9e8897d1db4b
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-31 17:31:33 -0400

    added validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -61,7 +61,7 @@ const pizzaController = {
         Pizza.findOneAndUpdate(
             { _id: params.id },
              body, 
-            { new: true })
+            { new: true, runValidators: true })
           .then(dbPizzaData => {
             if (!dbPizzaData) {
               res.status(404).json({ message: 'No pizza found with this id!' });
