99a9ad7e493b83afc8a4bc00c0410f04be169a28
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-08-31 19:07:56 -0400

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 //tells Mongoose which database we want to connect to
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
